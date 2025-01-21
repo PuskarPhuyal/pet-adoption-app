c6597c40e021fb219ef8a89eec60e0f2c9573580
Puskar Phuyal <puskarphuyal458@gmail.com>
2025-01-21 22:41:17 +0545

    update colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,12 +1,12 @@
 export default{
     // PRIMARY:'#E8B20E',
     // GRAY:'#8F8e8d',
-    // WHITE:'#fff',
+    WHITE:'#fff',
     // SECONDARY:'#4faaff',
     // LIGHT_PRIMARY:'#fff1c9'
     PRIMARY: '#FF6F61', // Vibrant coral
     GRAY: '#6C757D', // Cool slate gray
-    WHITE: '#FAF3F3', // Soft blush white
+    // WHITE: '#FAF3F3', // Soft blush white
     SECONDARY: '#7FD4FF', // Soft teal blue
     LIGHT_PRIMARY: '#D4FFD4' // Fresh mint green
 }
